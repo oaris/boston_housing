054ad1f169bb2c5121c01a91119cbc49ba6c54e1
Omar <omar.alrowais@gmail.com>
2018-10-22 00:54:23 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 17:48:09 2018
+
+@author: OOOOO
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+import random
+
+import visuals as vs
+
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+
+# Load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis = 1)
+    
+# Success
+print("Boston housing dataset has {} data points with {} variables each.".format(*data.shape))
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between 
+        true and predicted values based on the metric chosen. """
+    
+    # TODO: Calculate the performance score between 'y_true' and 'y_predict'
+    score =  r2_score(y_true, y_predict)
+    
+    # Return the score
+    return score
+
+score = performance_metric([3, -0.5, 2, 7, 4.2], [2.5, 0.0, 2.1, 7.8, 5.3])
+print("Model has a coefficient of determination, R^2, of {:.3f}.".format(score))
+
+random.seed(40)
+
+X_train, X_test, y_train, y_test = train_test_split(features, prices,  test_size=0.2, random_state=40)
+print("Training and testing split was successful.")
+
+#vs.ModelLearning(features, prices)
+
+# TODO: Import 'make_scorer', 'DecisionTreeRegressor', and 'GridSearchCV'
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import GridSearchCV
+
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a 
+        decision tree regressor trained on the input data [X, y]. """
+    
+    # Create cross-validation sets from the training data
+    cv_sets = ShuffleSplit(n_splits = 10, test_size = 0.20, random_state = 0)
+
+    # TODO: Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # TODO: Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    params = {'max_depth':[x for x in range(1,11)]}
+
+    # TODO: Transform 'performance_metric' into a scoring function using 'make_scorer' 
+    scoring_fnc = make_scorer(performance_metric)
+
+    # TODO: Create the grid search cv object --> GridSearchCV()
+    # Make sure to include the right parameters in the object:
+    # (estimator, param_grid, scoring, cv) which have values 'regressor', 'params', 'scoring_fnc', and 'cv_sets' respectively.
+    grid = GridSearchCV(regressor, params, scoring=scoring_fnc,cv ='cv_sets')
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
+
+# Fit the training data to the model using grid search
+reg = fit_model(X_train, y_train)
+
+# Produce the value for 'max_depth'
+print("Parameter 'max_depth' is {} for the optimal model.".format(reg.get_params()['max_depth']))
